d8d37d94ac3b7968181b1d9cf91d74172875d592
Keven <hkeven89@gmail.com>
2026-03-08 16:11:51 -0400

    deploy

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import styles from "./App.module.scss"
 
 function App() {
   return (
-    <div className={styles.root}></div>
+    <div className={styles.root}>
+      <h1>Hello World</h1>
+    </div>
   )
 }
 
